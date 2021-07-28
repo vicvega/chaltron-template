dc5de919a2f0df84f1bdcc3d1b519688dd8a4ecc
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2021-07-28 12:35:59 +0200

    fix badge classes

--- templates/app/helpers/chaltron/ldap_helper.rb
+++ templates/app/helpers/chaltron/ldap_helper.rb
@@ -4,13 +4,13 @@ module Chaltron
       capture do
         concat entry.name
         if User.exists?(username: entry.username)
-          concat tag.span(I18n.t('chaltron.users.already_present'), class: 'badge badge-danger ml-1')
+          concat tag.span(I18n.t('chaltron.users.already_present'), class: 'badge bg-danger ml-1')
         end
       end
     end
 
     def display_entry_email(entry)
-      entry.email.presence || tag.span(I18n.t('chaltron.users.missing_field'), class: 'badge badge-danger')
+      entry.email.presence || tag.span(I18n.t('chaltron.users.missing_field'), class: 'badge bg-danger')
     end
   end
 end
