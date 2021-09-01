4dfaff7e8be29f64888cc544040aa16943ec9aaa
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2021-09-01 12:35:36 +0200

    fix Chaltron::Log factory

--- templates/test/factories/chaltron/logs.rb
+++ templates/test/factories/chaltron/logs.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :chaltron_log, class: 'Chaltron::Log' do
     message { FFaker::HipsterIpsum.paragraph }
     severity { Chaltron::Log::SEVERITIES.sample }
-    category { FFaker::Company.bs }
+    category { %w[login user_admin].sample }
   end
 end
