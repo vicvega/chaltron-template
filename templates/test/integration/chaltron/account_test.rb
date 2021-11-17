689a705c092feda755fb1c0d5c11a4a664e3f852
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2021-11-17 17:00:55 +0100

    fix test

--- templates/test/integration/chaltron/account_test.rb
+++ templates/test/integration/chaltron/account_test.rb
@@ -8,22 +8,22 @@ class AccountTest < ActionDispatch::IntegrationTest
 
   test 'user should edit account data' do
     get root_url
-    assert_select "a[href='#{self_show_chaltron_users_path}']", I18n.t('chaltron.menu.self_show')
+    assert_select "a[href='#{chaltron_self_user_show_path}']", I18n.t('chaltron.menu.self_show')
 
-    get self_show_chaltron_users_path
+    get chaltron_self_user_show_path
     assert_select 'li.list-group-item' do
       assert_select 'strong', @user.email
     end
-    assert_select "a[href='#{self_edit_chaltron_users_path}']", I18n.t('chaltron.users.self_show.edit')
+    assert_select "a[href='#{chaltron_self_user_edit_path}']", I18n.t('chaltron.self_user.show.edit')
 
-    get self_edit_chaltron_users_path
-    assert_select "form#edit_chaltron_user[action='#{self_update_chaltron_users_path}']" do
+    get chaltron_self_user_edit_path
+    assert_select "form#edit_chaltron_user[action='#{chaltron_self_user_update_path}']" do
       assert_select "input[name='chaltron_user[fullname]'][value='#{@user.fullname}']"
       assert_select "input[name='chaltron_user[email]'][value='#{@user.email}']"
     end
 
     new_email = 'test@hogwarts.co.uk'
-    patch self_update_chaltron_users_path, params: {
+    patch chaltron_self_user_update_path, params: {
       chaltron_user: {
         email: new_email
       }
@@ -39,21 +39,21 @@ class AccountTest < ActionDispatch::IntegrationTest
 
   test 'user should change password' do
     get root_url
-    assert_select "a[href='#{self_show_chaltron_users_path}']", I18n.t('chaltron.menu.self_show')
+    assert_select "a[href='#{chaltron_self_user_show_path}']", I18n.t('chaltron.menu.self_show')
 
-    get self_show_chaltron_users_path
+    get chaltron_self_user_show_path
     assert_select 'li.list-group-item' do
       assert_select 'strong', @user.email
     end
-    assert_select "a[href='#{self_edit_chaltron_users_path}']", I18n.t('chaltron.users.self_show.edit')
+    assert_select "a[href='#{chaltron_self_user_edit_path}']", I18n.t('chaltron.self_user.show.edit')
 
-    get change_password_chaltron_users_path
-    assert_select "form#edit_chaltron_user[action='#{self_update_chaltron_users_path}']" do
+    get chaltron_self_user_change_password_path
+    assert_select "form#edit_chaltron_user[action='#{chaltron_self_user_update_path}']" do
       assert_select "input[name='chaltron_user[password]']"
       assert_select "input[name='chaltron_user[password_confirmation]']"
     end
     new_password = 'password.1'
-    patch self_update_chaltron_users_path, params: {
+    patch chaltron_self_user_update_path, params: {
       chaltron_user: {
         password: new_password,
         password_confirmation: new_password
