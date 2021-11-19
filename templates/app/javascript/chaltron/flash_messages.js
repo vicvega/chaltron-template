83efde88d40f1bdbcb78d7a082dbe110c5c73e97
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2021-11-19 13:56:35 +0100

    fix issue with disappered flash messages

--- templates/app/javascript/chaltron/flash_messages.js
+++ templates/app/javascript/chaltron/flash_messages.js
@@ -1,12 +1,7 @@
 document.addEventListener('turbo:load', () => {
-  const disappear = function f(e) {
-    const element = e;
-    element.style.opacity = '0';
-  };
-  // flash messages
   const flash = document.querySelectorAll('.flash-container div.alert');
   if (flash.length > 0) {
-    flash.forEach((f) => f.addEventListener('click', () => disappear(f)));
-    setTimeout(() => flash.forEach((f) => disappear(f)), 5000);
+    flash.forEach((f) => f.addEventListener('click', () => f.remove()));
+    setTimeout(() => flash.forEach((f) => f.remove()), 5000);
   }
 });
