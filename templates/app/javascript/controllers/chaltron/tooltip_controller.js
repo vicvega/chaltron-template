2fbe81877e99665761c887ec5bb9dfd2e55b9ff1
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2022-01-29 17:47:03 +0100

    minor refactor in tooltip controller

--- templates/app/javascript/controllers/chaltron/tooltip_controller.js
+++ templates/app/javascript/controllers/chaltron/tooltip_controller.js
@@ -3,7 +3,6 @@ import * as bootstrap from 'bootstrap';
 
 export default class extends Controller {
   connect() {
-    const list = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    list.map((tooltip) => new bootstrap.Tooltip(tooltip));
+    new bootstrap.Tooltip(this.element);
   }
 }
