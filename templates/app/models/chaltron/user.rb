5006adeee0bbb5496792b01c8d02e729f80b84c6
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2022-05-04 16:51:37 +0200

    add sti for devise models

--- templates/app/models/chaltron/user.rb
+++ templates/app/models/chaltron/user.rb
@@ -1,11 +1,6 @@
 module Chaltron
   class User < ApplicationRecord
-    include Enableable
     include HasRoles
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :recoverable, :rememberable, :validatable,
-           :timeoutable, :trackable, :omniauthable
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }
     # Only allow letter, number, underscore and punctuation.
@@ -15,8 +10,6 @@ module Chaltron
 
     has_one_attached :avatar
 
-    attr_writer :login
-
     def self.search(search)
       if search
         where('username LIKE :query or fullname LIKE :query or email LIKE :query', { query: "%#{search}%" })
@@ -25,36 +18,8 @@ module Chaltron
       end
     end
 
-    def login
-      @login || username || email
-    end
-
     def display_name
       fullname.presence || username
-    end
-
-    def ldap_user?
-      provider == 'ldap'
-    end
-
-    # override devise method to allow sign in with email or username
-    def self.find_for_database_authentication(warden_conditions)
-      conditions = warden_conditions.dup
-      if (login = conditions.delete(:login))
-        where(conditions.to_h)
-          .find_by(['lower(username) = :value OR lower(email) = :value', { value: login.downcase }])
-      elsif conditions.key?(:username) || conditions.key?(:email)
-        find_by(conditions.to_h)
-      end
-    end
-
-    # override devise method to check if user account is active
-    def active_for_authentication?
-      super && enabled?
-    end
-
-    def inactive_message
-      enabled? ? super : I18n.t('chaltron.users.inactive_message')
     end
 
     def avatar_variable
--- templates/app/models/concerns/chaltron/enableable.rb
+++ templates/app/models/concerns/chaltron/enableable.rb
@@ -22,5 +22,14 @@ module Chaltron
     def disable!
       update!(enabled: false)
     end
+
+    # override devise method to check if user account is active
+    def active_for_authentication?
+      super && enabled?
+    end
+
+    def inactive_message
+      enabled? ? super : I18n.t('chaltron.users.inactive_message')
+    end
   end
 end
