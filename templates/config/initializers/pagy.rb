b7147ed4bc61d2cededcb49656f28477b6f66b1e
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2022-08-05 11:25:39 +0200

    comment out pagy localization

--- templates/config/initializers/pagy.rb
+++ templates/config/initializers/pagy.rb
@@ -2,4 +2,4 @@ require 'pagy/extras/bootstrap'
 require 'pagy/extras/overflow'
 
 Pagy::DEFAULT[:overflow] = :last_page
-Pagy::I18n.load(locale: 'it', filepath: Rails.root.join('config/locales/pagy.it.yml'))
+#Pagy::I18n.load(locale: 'it', filepath: Rails.root.join('config/locales/pagy.it.yml'))
