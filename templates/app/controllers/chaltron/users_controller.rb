e20ad6a907b3a855ba3f59ba3a770268d07e5f05
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2023-11-29 14:34:31 +0100

    housekeeping

--- templates/app/controllers/chaltron/users_controller.rb
+++ templates/app/controllers/chaltron/users_controller.rb
@@ -4,9 +4,7 @@ module Chaltron
     include Sortable
     include Searchable
 
-    preserve :filter, allow_blank: true
-    # preserve :filter, :page, :per_page, :sort_direction, :sort_column
-    preserve :search, allow_blank: true
+    preserve :filter, :search, allow_blank: true
 
     # searchable strip: false
 
