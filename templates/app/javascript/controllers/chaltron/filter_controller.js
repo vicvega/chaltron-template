59438a78510e5f0233348917a62b3232ec907946
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2024-01-31 12:31:11 +0100

    refactor automatic filter submission

--- templates/app/javascript/controllers/chaltron/filter_controller.js
+++ templates/app/javascript/controllers/chaltron/filter_controller.js
@@ -1,17 +1,18 @@
 import { Controller } from '@hotwired/stimulus'
-import { Turbo } from '@hotwired/turbo-rails'
 
-// Connects to data-controller="chaltron--form"
+// Connects to data-controller="chaltron--filter"
 export default class extends Controller {
-  static values = { frame: String }
+  connect () {
+    if (this.element.nodeName !== 'FORM') {
+      console.error('chaltron--filter stimulus controller must be referenced by a FORM tag')
+    } else {
+      Array.from(this.element.elements).forEach((input) => {
+        input.setAttribute('data-action', 'chaltron--filter#submit')
+      })
+    }
+  }
 
   submit () {
-    const form = this.element
-    const formData = new window.FormData(form)
-
-    const params = new URLSearchParams(formData)
-    const url = `${form.action}?${params.toString()}`
-
-    Turbo.visit(url, { frame: this.frameValue })
+    this.element.requestSubmit()
   }
 }
