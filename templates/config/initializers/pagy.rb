1898bd6167849954961ba58fd918db1dca6a6b59
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2024-02-22 15:30:24 +0100

    remove unuseful options for pagy

--- templates/config/initializers/pagy.rb
+++ templates/config/initializers/pagy.rb
@@ -1,5 +1,1 @@
 require "pagy/extras/bootstrap"
-require "pagy/extras/overflow"
-
-Pagy::DEFAULT[:overflow] = :last_page
-# Pagy::I18n.load(locale: "it", filepath: Rails.root.join("config/locales/pagy.it.yml"))
