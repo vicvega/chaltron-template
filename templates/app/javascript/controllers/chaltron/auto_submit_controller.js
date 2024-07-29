e9d6224e7ec31b4f78a84c0f870922a93d3866ae
Francesco Coda Zabetta <francesco.codazabetta@gmail.com>
2024-07-29 13:15:23 +0200

    rename auto-sumbit stimulus controller

--- templates/app/javascript/controllers/chaltron/auto_submit_controller.js
+++ templates/app/javascript/controllers/chaltron/auto_submit_controller.js
@@ -1,13 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
 
-// Connects to data-controller="chaltron--filter"
+// Connects to data-controller="chaltron--auto-submit"
 export default class extends Controller {
   connect () {
     if (this.element.nodeName !== 'FORM') {
-      console.error('chaltron--filter stimulus controller must be referenced by a FORM tag')
+      console.error('chaltron--auto-submit stimulus controller must be referenced by a FORM tag')
     } else {
       Array.from(this.element.elements).forEach((input) => {
-        this.#appendAction(input, 'chaltron--filter#submit')
+        this.#appendAction(input, 'chaltron--auto-submit#submit')
       })
     }
   }
